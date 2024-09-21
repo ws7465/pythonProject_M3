4020078d07a391b2642998db9d4be5250ec511e5
ws7465 <ws7465@ya.ru>
2024-09-21 21:21:45 +0300

    модуль 3-2 20240921 21:21

--- main.py
+++ main.py
@@ -33,4 +33,20 @@
 #     Вместо `my_list` нужно подставить исходный список строк.
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-print('Capybara')
+# print('Capybara')
+
+
+
+def func_with_params(a, b=2, c=None):
+    if c is None:
+        c = []
+        c.append(a)
+    print(c)
+
+
+
+func_with_params(3)
+func_with_params(4)
+func_with_params(5)
+func_with_params(6)
+
