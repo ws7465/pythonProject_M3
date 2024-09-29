21a52efbea85aefae26c426833fb5c9bad9fdf96
ws7465 <ws7465@ya.ru>
2024-09-29 11:27:48 +0300

    модуль ДОП 20240929 11:27 (калькулятор)

--- DOP.py
+++ DOP.py
@@ -54,19 +54,23 @@ window.mainloop()
 #                                   изменении размера окна не сбивались элементы
 #                                   созданные методом .place )
 # button_add = tk.Button(window, text="+", width=2, height=2, command=add)  # создаем
-#                                   переменную - кнопку "button_add" с помощью метода
-#                                   Button библиотеки tk. Кнопка принадлежит к окну
-#                                   window, на ней надпись +, ширина 2, высота 2,
+#                                   переменную - кнопку "button_add" (ПРИБАВИТЬ) с
+#                                   помощью метода Button библиотеки tk.
+#                                   Кнопка принадлежит к окну window,
+#                                   на ней надпись + , ширина 2, высота 2,
 #
 # button_add.place(x=100, y=200)  # размещаем кнопку по координатам 100 и 200
 #                                   (кроме метода .place есть ещё два метода
 #                                   размещения ВИДЖЕТОВ (кнопок, окошечек ввода и т.п.)
 #                                   в библиотеке tk -это для самостоятельного изучения)
-# button_sub = tk.Button(window, text="-", width=2, height=2, command=sub)  #
+# button_sub = tk.Button(window, text="-", width=2, height=2, command=sub)  # виджет -
+#                                                                      кнопка "минус"
 # button_sub.place(x=150, y=200)  #
-# button_mul = tk.Button(window, text="*", width=2, height=2, command=mul)  #
+# button_mul = tk.Button(window, text="*", width=2, height=2, command=mul)  # виджет -
+#                                                                    кнопка "умножить"
 # button_mul.place(x=200, y=200)  #
-# button_div = tk.Button(window, text="/", width=2, height=2, command=div) #
+# button_div = tk.Button(window, text="/", width=2, height=2, command=div) # виджет -
+#                                                                   кнопка "разделить"
 # button_div.place(x=250, y=200)  #
 # number1_entry = tk.Entry(window, width=28)  #
 # number1_entry.place(x=100, y=75)  #
